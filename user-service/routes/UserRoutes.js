a3195b4c9b9f6c25ef8052c4e14a9488a870453f
Harshit Khatri <62706135+harshit-k11@users.noreply.github.com>
2023-12-05 17:51:41 +0000

    Setup ESLint

--- user-service/routes/UserRoutes.js
+++ user-service/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController');
 
-// Define the user registration route
+// Define the user login and registration route
 router.post('/login', userController.loginUser);
 router.post('/register', userController.registerUser);
 
